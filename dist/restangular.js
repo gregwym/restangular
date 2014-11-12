152c29310bc8f6106c55385209f4d39e6fd82471
Greg Wang <greg.wang@genesys.com>
2014-11-12 16:23:47 -0500

    Fix #697: copyRestangularizedElement should keep original fromServer attribute.

--- dist/restangular.js
+++ dist/restangular.js
@@ -946,7 +946,8 @@ module.provider('Restangular', function() {
               function copyRestangularizedElement(fromElement, toElement) {
                   var copiedElement = angular.copy(fromElement, toElement);
                   return restangularizeElem(copiedElement[config.restangularFields.parentResource],
-                          copiedElement, copiedElement[config.restangularFields.route], true);
+                          copiedElement, copiedElement[config.restangularFields.route],
+                          copiedElement[config.restangularFields.fromServer]);
               }
 
               function restangularizeElem(parent, element, route, fromServer, collection, reqParams) {
